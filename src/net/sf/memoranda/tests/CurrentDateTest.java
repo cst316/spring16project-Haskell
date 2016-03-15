5ec0f441c9166db30c7c2cead411e5fdd474db0b
Gabriel Waltrip <Gabriel.Waltrip@gmail.com>
2016-03-14 20:12:54 -0700

    Test
    Some test classes

---
+++ src/net/sf/memoranda/tests/CurrentDateTest.java
@@ -0,0 +1,38 @@
+package net.sf.memoranda.tests;
+
+import static org.junit.Assert.*;            
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test; 
+import net.sf.memoranda.date.CurrentDate;
+ 
+public class CurrentDateTest
+{
+    private CurrentDate cd = null;
+	
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {
+        //TODO - Create setUp
+    }
+    
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception
+    {
+        //TODO - Create tearDown
+    }
+    
+    @Before
+    public void setUp() throws Exception
+    {
+        //TODO - Create setUp
+    }
+    
+    @After
+    public void tearDown() throws Exception
+    {
+        //TODO - Create tearDown
+    }
+}
